e3cf00fd749be75b2965331aedc07c844977d8eb
Alvaro <alvarodeguer@gmail.com>
2020-05-30 14:24:51 -0300

    Creada parte visual ,falta clases & metodos

---
+++ TP7/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP7
+{
+    public class Calculadora
+    {
+        float numero1;
+        float numero2;
+    }
+    //metodos//
+}
